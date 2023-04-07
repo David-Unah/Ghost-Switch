37f002636886d86a3371bf4167ddc9c7ee3d295b
David Unah <davidunah022@gmail.com>
2023-04-07 09:28:11 +0100

    updated menu links

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ let answers = document.querySelectorAll(".faqboxes");
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
   }
 
 showSlides();
